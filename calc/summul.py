a8b88bfd2de3d1ef76dba0c972c2d11b83607e51
3ltigr0 <vcz1017@gmail.com>
2020-06-28 16:22:46 +0900

    Issue #5 Add 'Please enter value' code in summul.py Add table width in templatesm.py

--- calc/summul.py
+++ calc/summul.py
@@ -5,17 +5,20 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    x = 'Empty'
-    y = 'Empty'
+    s = ''
+    t = ''
+    x = ''
+    y = ''
     if '' not in [a, b]:
         a = int(a)
         b = int(b)
         x = str(a+b)
         y = str(a*b)
-    response_body = html % (x, y) #+ 'sum: ' + str(x) + ' mul: ' + str(y)
+    else:
+        s = 'Please enter value'
+    response_body = html % (s, x, y) #+ 'sum: ' + str(x) + ' mul: ' + str(y)
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
     ])
     return [response_body]
-
--- calc/templatesm.py
+++ calc/templatesm.py
@@ -2,21 +2,23 @@ html = b"""
 <html>
     <body>
         <form action="">
-            <input type="number" name="a">
-            <input type="number" name="b">
-            <input type="submit"><br><br><br>
+            a: <input type="number" name="a">
+            b: <input type="number" name="b">
+            <input type="submit">
+            <br>
+            %s
+            <br><br>
         </form>
         <table border = "1">
             <tr>
                 <td><b>sum</b></td>
-                <td><b>%s</b></td>
+                <td width = "40">%s</td>
             </tr>
             <tr>
                 <td><b>mul</b></td>
-                <td><b>%s</b></td>
+                <td width = "40">%s</td>
             </tr>
         </table>
     </body>
 </html>
 """
-
